465270324ce1816423a8936cabb9e775ed64371f
shved-1682 <vladislav.prm@gmail.com>
2022-01-10 18:09:25 +0500

    methods DONE: print_one, method_two, method_three

--- main.py
+++ main.py
@@ -10,10 +10,22 @@ def bye_world():
     print("Goodbye, World!")
 
 
-def print_one():
-    print("1")
+def f_12345():
+    def print_one():
+        print("1")
+
+    def print_two():
+        print("2")
+
+    def print_three():
+        print("3")
+
+    print_one()
+    print_two()
+    print_three()
 
 
 hello_world()
 bye_world()
 print_hello()
+f_12345()
